713bd0015b4734070d5eec41046ac7e1a25581c5
Tarini Singh <tarsingh@fb.com>
2018-07-05 10:54:36 -0700

    handle has @ symbol in front

--- twitter/Views/TweetCellTableViewCell.m
+++ twitter/Views/TweetCellTableViewCell.m
@@ -21,8 +21,7 @@
     _tweet = tweet;
     
     self.firstLast_label.text = self.tweet.user.name;
-    self.handle_label.text =
-    self.tweet.user.screen_name;
+    self.handle_label.text = [NSString stringWithFormat:@"@%@", self.tweet.user.screen_name];
     self.date_label.text = self.tweet.createdAtString;
     self.tweet_label.text = self.tweet.text;
     self.retweet_label.text = [NSString stringWithFormat:@"%i", self.tweet.retweetCount];
