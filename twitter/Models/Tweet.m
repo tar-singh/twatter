f357187f75a92ca96dc9d992684aa678141e8710
Tarini Singh <tarsingh@fb.com>
2018-07-05 23:01:22 -0700

    displays relative timestamp for each tweet

--- twitter/Models/Tweet.m
+++ twitter/Models/Tweet.m
@@ -8,6 +8,7 @@
 
 #import "Tweet.h"
 #import "User.h"
+#import "DateTools.h"
 
 @implementation Tweet
 
@@ -43,11 +44,9 @@
         formatter.dateFormat = @"E MMM d HH:mm:ss Z y";
         // Convert String to Date
         NSDate *date = [formatter dateFromString:createdAtOriginalString];
-        // Configure output format
-        formatter.dateStyle = NSDateFormatterShortStyle;
-        formatter.timeStyle = NSDateFormatterNoStyle;
+        
         // Convert Date to String
-        self.createdAtString = [formatter stringFromDate:date];
+        self.createdAtString = [NSString stringWithFormat:@"%@", date.shortTimeAgoSinceNow];
     }
     return self;
 }
